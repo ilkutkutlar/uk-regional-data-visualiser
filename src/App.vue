0a9a7c23e391414c5c79051f16420fe10ce3555d
Ilkut Kutlar <ilkutkutlar@gmail.com>
2023-01-02 16:17:38 +0000

    display values from 2 other years in the info panel as well

--- src/App.vue
+++ src/App.vue
@@ -29,13 +29,53 @@ export default {
     data() {
       return this.dataset.data[this.timePeriod];
     },
+    otherTimePeriods() {
+      const currentIndex = this.dataset.timePeriods.indexOf(this.timePeriod);
+      const timePeriods = this.dataset.timePeriods;
+      if (currentIndex == 0) {
+        if (timePeriods.length >= 3) {
+          return [
+            timePeriods[currentIndex + 2],
+            timePeriods[currentIndex + 1],
+            this.timePeriod,
+          ];
+        } else if (timePeriods.length == 2) {
+          return [timePeriods[currentIndex + 1], this.timePeriod];
+        } else {
+          return [this.timePeriod];
+        }
+      } else if (currentIndex == 1) {
+        if (timePeriods.length >= 3) {
+          return [
+            timePeriods[currentIndex + 1],
+            this.timePeriod,
+            timePeriods[currentIndex - 1],
+          ];
+        } else {
+          return [this.timePeriod, this.dataset.timePeriods[currentIndex - 1]];
+        }
+      } else {
+        return [
+          this.timePeriod,
+          this.dataset.timePeriods[currentIndex - 1],
+          this.dataset.timePeriods[currentIndex - 2],
+        ];
+      }
+    },
     infoPanelBodyText() {
-      const valueThisYear = this.dataset.valueForArea(
-        this.timePeriod,
-        this.infoPanelRegionId,
-        true
-      );
-      return `<span class='fw-bold'>${this.timePeriod}</span>: ${valueThisYear}`;
+      return this.otherTimePeriods
+        .map((year) => {
+          const value = this.dataset.valueForArea(
+            year,
+            this.infoPanelRegionId,
+            true
+          );
+          if (year == this.timePeriod) {
+            return `<div style="background-color: lightgrey; padding-top: 0.3em; padding-bottom: 0.3em"><span class='fw-bold'>${year}</span>: ${value}</div>`;
+          }
+          return `<div><span class='fw-bold'>${year}</span>: ${value}</div>`;
+        })
+        .join("");
     },
   },
   watch: {
