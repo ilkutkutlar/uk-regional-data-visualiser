e4444ad263e30996ef15971b5751e369d8f3c812
Ilkut Kutlar <ilkutkutlar@gmail.com>
2025-03-23 22:07:51 +0000

    upgrade dev dependencies

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from "@eslint/js";
+import pluginVue from 'eslint-plugin-vue'
+import prettierConfig from "@vue/eslint-config-prettier";
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs,
+} from '@vue/eslint-config-typescript'
+
+export default defineConfigWithVueTs(
+  js.configs.recommended,
+  pluginVue.configs['flat/recommended'],
+  prettierConfig,
+  vueTsConfigs.recommended,
+  { ignores: ["node_modules/**", "dist/", ".gitignore"] },
+  {
+    files: [
+      "**/*.vue",
+      "**/*.js",
+      "**/*.jsx",
+      "**/*.cjs",
+      "**/*.mjs",
+      "**/*.ts",
+      "**/*.tsx",
+      "**/*.cts",
+      "**/*.mts",
+    ],
+    languageOptions: {
+			ecmaVersion: "latest",
+    },
+    rules: {
+      complexity: "warn",
+      "dot-notation": "warn",
+      eqeqeq: "error",
+      "func-style": "warn",
+      "new-cap": "warn",
+      "no-console": "error",
+      "no-duplicate-imports": "error",
+      "no-extra-bind": "warn",
+      "no-use-before-define": "error",
+      "sort-imports": ["error", { ignoreCase: true, allowSeparatedGroups: true }],
+    },
+  }
+);
