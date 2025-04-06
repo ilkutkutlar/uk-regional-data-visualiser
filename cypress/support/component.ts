de0b48d9bfb11253c01cac65329df9b2426dd514
Ilkut Kutlar <ilkutkutlar@gmail.com>
2025-04-06 13:23:15 +0100

    fix ESLint warnings

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,24 +1,5 @@
-// ***********************************************************
-// This example support/component.ts is processed and
-// loaded automatically before your test files.
-//
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
-//
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
-//
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
-
-// Import commands.js using ES2015 syntax:
+/* eslint-disable @typescript-eslint/no-namespace */
 import "./commands";
-
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
 import { mount } from "cypress/vue";
 
 // Augment the Cypress namespace to include type definitions for
@@ -34,6 +15,3 @@ declare global {
 }
 
 Cypress.Commands.add("mount", mount);
-
-// Example use:
-// cy.mount(MyComponent)
