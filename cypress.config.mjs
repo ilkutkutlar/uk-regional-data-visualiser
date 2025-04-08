9278efa4e83e38e86b36e09a8d36ca4a366d5da9
Ilkut Kutlar <ilkutkutlar@gmail.com>
2025-04-08 22:10:07 +0100

    move end to end tests to under tests directory

--- cypress.config.mjs
+++ cypress.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     baseUrl: "https://localhost:5173",
+    supportFile: false,
+    specPattern: "tests/e2e/**/*.cy.{js,jsx,ts,tsx}",
   },
   component: {
     devServer: {
