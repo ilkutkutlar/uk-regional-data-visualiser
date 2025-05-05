8514484bb4840c6881f60924701e1f0ac1e5456c
Ilkut Kutlar <ilkutkutlar@gmail.com>
2025-05-05 21:58:40 +0100

    remove unnecessary fields from GeoJSON files

--- scripts/shp_file_to_geojson.sh
+++ scripts/shp_file_to_geojson.sh
@@ -1,5 +1,7 @@
 shp_file="$1"
 id_field="$2"
+name_field="$3"
+native_name_field="$4"
 
 output_dir="$(dirname "${shp_file}")"
 output_file="$(basename "${shp_file}" | sed 's/.zip/.geojson/g')"
@@ -11,4 +13,6 @@ SIMPLIFY_FACTOR='25.0%'
 # -proj webmercator \
 mapshaper "${shp_file}" \
   -simplify weighted "${SIMPLIFY_FACTOR}" \
+  -filter-fields "${id_field},${name_field},${native_name_field}" \
+  -rename-fields "name=${name_field},namew=${native_name_field},id=${id_field}" \
   -o format=geojson id-field="${id_field}" "${output_dir}/${output_file}"
